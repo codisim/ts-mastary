2e4ff2a7ba302dadd606e315a1ce36e6a56ca375
MD Waliullah <waliullah.godev@gmail.com>
2026-01-17 01:31:40 +0600

    create fakedb for performe with typeAlias

---
+++ src/fakedb.ts
@@ -0,0 +1,54 @@
+ const userData = `
+    [
+        {
+            "id": 1,
+            "firstName": "John",
+            "lastName": "Doe",
+            "skills": [
+                {
+                    "name": "TypeScript",
+                    "level": "expert",
+                    "rating": 5
+                },
+                {
+                    "name": "JavaScript",
+                    "level": "expert",
+                    "rating": 5
+                }
+            ],
+            "address": {
+                "street": "123 Main St",
+                "city": "Anytown",
+                "country": "USA",
+                "zipCode": 12345,
+                "position": {
+                    "lat": 40.7128,
+                    "lng": -74.0060
+                }
+            }
+        },
+        {
+            "id": 2,
+            "firstName": "Jane",
+            "skills": [
+                {
+                    "name": "Python",
+                    "level": "intermediate",
+                    "rating": 4
+                }
+            ],
+            "address": {
+                "street": "456 Oak St",
+                "city": "Othertown",
+                "country": "USA",
+                "position": {
+                    "lat": 34.0522,
+                    "lng": -118.2437
+                }
+            }
+        }
+    ]       
+`
+
+
+export default userData;
--- src/typeAlias2.ts
+++ src/typeAlias2.ts
@@ -2,16 +2,25 @@ type User = {
     id: string | number;
     firstName: string;
     lastName?: string;
-    skills: string[];
+    skills: [{
+        name: string;
+        level: 'beginner' | 'intermediate' | 'expert';
+        rating: number;
+    }];
     address: {
         street: string;
         city: string;
         country: string;
         zipCode?: number;
+        position: {
+            lat: number;
+            lng: number;
+        }
     }
 }
 
 type UserAddress = User['address'];
+type UserAddressPosition = User['address']['position']
 
 function getAddress(add: UserAddress) {
 
