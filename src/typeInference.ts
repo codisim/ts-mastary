2be637c35c1c7397aebbc9f87190567f16afcc18
MD Waliullah <waliullah.godev@gmail.com>
2026-01-16 21:38:51 +0600

    typeinference

--- src/typeInference.ts
+++ src/typeInference.ts
@@ -4,3 +4,23 @@ const names = ['one', 'two', 'three', 'four', 'five'];
 
 
 const something = [1, 2, 3, 4, 'five', 'six', true, false];
+
+
+
+function add(a = 0, b = 0) {
+    return a + b;
+}
+
+add(5, 10);
+
+
+
+const user = {
+    id: 1,
+    name: 'Alice',
+    isVerified: true,
+}
+
+function printUserInfo(inputValue: typeof user){
+    
+}
