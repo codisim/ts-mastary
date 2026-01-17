2fa1492a3b33705b725083f11a64703313a562af
MD Waliullah <waliullah.godev@gmail.com>
2026-01-17 14:29:56 +0600

    refactor: comment out console log in profile function and add mergeObj function with examples

--- src/generic.ts
+++ src/generic.ts
@@ -1,5 +1,5 @@
 function profile<T>(value: T): T {
-    console.log(value, typeof value);
+    // console.log(value, typeof value);
     return value;
 }
 
@@ -10,3 +10,18 @@ profile<number>(42);
 profile<boolean>(true);
 
 profile<{ name: string; age: number }>({ name: "Alice", age: 30 });
+
+
+
+
+
+function mergeObj<T, U>(obj: T, obj2: U): T & U {
+    return { ...obj, ...obj2 }
+}
+
+
+const merged = mergeObj({ name: "Alice" }, { age: 30, city: "New York" });
+const merged2 = mergeObj({ id: 1, title: "Book" }, { city: "New York", author: "John Doe", pages: 250 });
+
+console.log(merged);
+console.log(merged2);
